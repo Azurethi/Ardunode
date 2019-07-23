f5d2960bd39ff1185f5c33ab4c8a6c31a38e1cfe
Azurethi <github@azurethi.me>
2019-07-23 10:15:07 +0100

    added arduino-side usage example

---
+++ arduino/example.ino
@@ -0,0 +1,48 @@
+//Packet Data Serial write function
+/* Usage: pd( <variable to write> ) 
+ * 
+ * NB: types & packet definitions must be registered pc-side
+ *
+ * int, long and float are implemented by default, along with the three debug packets below
+ */
+// ignore from here
+#define pd(a) swrite((byte *)&a,sizeof(a))
+#define pid(a) Serial.write(a)
+void swrite(byte *b, int l){for(int i =0; i<l;i++)pid(b[i]);}
+// ignore to here
+
+
+//Define your packets!
+/* First send a Packet id using: pid(<id>);
+ *  add packet data with one or more: pd(<data>);
+ */
+void packet_debugInt(int i) {
+    pid(0xFD); pd(i);
+}
+
+void packet_debugLong(long l) {
+    pid(0xFE); pd(l);
+}
+void packet_debugFloat(float f) {
+    pid(0xFF); pd(f);
+}
+
+void packet_example(int i, long l, float f){    //not implemented pc-side by default. Structure would be ['int','long','float']
+    pid(0x01); pd(i); pd(l); pd(f);
+}
+//end of packet definitions
+
+
+//regular arduino code
+void setup() {
+    Serial.begin(112500);
+}
+
+void loop() {
+
+    packet_debugInt(1234);
+    packet_debugLong(135325L);
+    packet_debugFloat(124145.512f);
+    delay(1000);
+}
+
